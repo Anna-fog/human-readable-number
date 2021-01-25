2eaa4b2d30262e9ef5d8e1c7cd35de966105ac17
Anna-fog <anna49.fog@yandex.ru>
2021-01-25 09:44:35 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+
+    if(!number || number === 0) return 'zero';
+
+    const oneToTwenty = ['','one','two','three','four','five','six','seven','eight','nine','ten',
+        'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tens = ['', '', 'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+
+    const num = ('0000000'+ number).slice(-6).match(/^(\d{1})(\d{2})(\d{1})(\d{2})$/);
+    if(!num) return;
+
+    let res = num[1] != 0 ? oneToTwenty[Number(num[1])] + ' hundred ' : '';
+    res += num[2] != 0 ? (oneToTwenty[Number(num[2])] || `${tens[num[2][0]]} ${oneToTwenty[num[2][1]]}`) + ' thousand ' : '';
+    res += num[3] != 0 ? oneToTwenty[Number(num[3])] + ' hundred ' : '';
+    res += num[4] != 0 ? (oneToTwenty[Number(num[4])] || `${tens[num[4][0]]} ${oneToTwenty[num[4][1]]}`) + '' : '';
+
+    return res.trim();
 }
